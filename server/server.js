75261f043abf09beb6ca7e4a4d8a1784da424bf6
giri <girikrishna.p6@gmail.com>
2025-03-25 11:09:03 +0530

    app to server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,38 @@
-// Entry point for the server
-const app = require('./app');
+const express = require('express');
+const cors = require('cors');
+const { errorHandler } = require('./middlewares/errorMiddleware');
+const authRoutes = require('./routes/authRoutes');
+const testRoutes = require('./routes/testRoutes');
+const feedbackRoutes = require('./routes/feedbackRoutes');
 const config = require('./config/config');
 const connectDB = require('./config/db');
 
-// Connect to database
+// Initialize express app
+const app = express();
+// Middleware
+app.use(cors());
 connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use('/auth', authRoutes);
+app.use('/questions', testRoutes);
+app.use('/feedback', feedbackRoutes);
+
+
+// Error handling middleware
+app.use(errorHandler);
+
+// Handle 404 routes
+app.use('*', (req, res) => {
+    res.status(404).json({
+        status: 'error',
+        message: `Can't find ${req.originalUrl} on this server!`
+    });
+});
+
 
 // Start the server
 const PORT = config.PORT || 5000;
