c9a0786a31ea3946dae6d39bcf677a208050358a
giri <girikrishna.p6@gmail.com>
2025-03-25 12:19:17 +0530

    net

--- server/functions/server.js
+++ server/functions/server.js
@@ -1,4 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const { errorHandler } = require("./middlewares/errorMiddleware");
+const authRoutes = require("./routes/authRoutes");
+const testRoutes = require("./routes/testRoutes");
+const feedbackRoutes = require("./routes/feedbackRoutes");
+const connectDB = require("./config/db");
 const serverless = require("serverless-http");
-const app = require("../server");
 
+// Initialize express app
+const app = express();
+
+// Middleware
+app.use(cors());
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use("/.netlify/functions/server/auth", authRoutes);
+app.use("/.netlify/functions/server/questions", testRoutes);
+app.use("/.netlify/functions/server/feedback", feedbackRoutes);
+
+// Error handling middleware
+app.use(errorHandler);
+
+// Handle 404 routes
+app.use("*", (req, res) => {
+    res.status(404).json({
+        status: "error",
+        message: `Can't find ${req.originalUrl} on this server!`,
+    });
+});
+
+// Export the app as a serverless function
 module.exports.handler = serverless(app);
